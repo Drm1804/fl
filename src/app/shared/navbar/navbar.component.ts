6db8c282a6a8188d32c20a746cdcad3c1f6d3fb0
Устинов Алексей Геннадьевич <ustinov@nports.ru>
2016-06-17 11:03:06 +0500

    авторизация для теста

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
 import {ROUTER_DIRECTIVES, Routes, Router} from '@angular/router';
+
+import {AuthService} from '../services/auth.services';
 
 @Component({
   moduleId: module.id,
   selector: 'navbar',
   templateUrl: 'navbar.component.html',
   styleUrls: ['navbar.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES],
+  providers: [AuthService]
 })
 export class NavbarComponent implements OnInit {
 
   title: string;
 
-  constructor(public router: Router) {
+  constructor(public router: Router, private authService:AuthService) {
     this.title = 'FL'
   }
 
@@ -25,7 +27,7 @@ export class NavbarComponent implements OnInit {
     return this.router.serializeUrl(this.router.urlTree) == this.router.serializeUrl((this.router.createUrlTree([route])));
   }
   logIn(){
-    console.log('asfsdfsdf')
+    this.authService.checkLoginStatus()
   }
 
 }
--- src/app/shared/services/auth.services.ts
+++ src/app/shared/services/auth.services.ts
@@ -1,15 +1,31 @@
 ///<reference path="../../globals.d.ts"/>
 import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class AuthService {
-
+  public users: any;
+  private _usersObserver: any;
+  private _users:any = [];
 
   constructor() {
     // this.users = new Observable(observer => {
     //   this._usersObserver = observer;
     // });
-    // this.init();
+  }
+
+
+  private getUsers () {
+    VK.Api.call('friends.get', {fields: ['photo_50','education']}, function(r){
+      console.log(r)
+      if(r.response) {
+        this._users = r.response;
+        console.log("Пользователи загружены!");
+        // this.fetchUsers();
+      }else{
+        console.log("Ошибка получения пользователей!");
+      }
+    });
   }
 
   // Инициализируем vk.com
@@ -17,11 +33,23 @@ export class AuthService {
       VK.init({
         apiId: 5419902
       });
-    // window.onload = function() {
+  }
 
-    //
-    //   // this.CheckStatusLogin();
-    // };
-
+  checkLoginStatus(){
+    VK.Auth.getLoginStatus(function(response){
+      if (response.session && localStorage.getItem('user.session')!== null) {
+        this.getUsers();
+      } else {
+        VK.Auth.login(function(response){
+          if (response.session) {
+            localStorage.setItem('user.session', JSON.stringify(response.session) );
+            this.getUsers();
+          } else {
+            console.log('/* Пользователь нажал кнопку Отмена в окне авторизации */');
+            return false;
+          }
+        });
+      }
+    });
   }
 }
