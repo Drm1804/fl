d3093c4cb00d49fb4e80fcb31ca33839cf3bca77
Устинов Алексей Геннадьевич <ustinov@nports.ru>
2016-06-17 10:18:28 +0500

    Сделал сортировку с помощью кастомного пайпа

--- src/app/friends.component/friends.component.ts
+++ src/app/friends.component/friends.component.ts
@@ -3,19 +3,23 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import { DataService } from '../shared/services/data.services';
 import { IFriend } from '../shared/interfaces';
+import { OrderFriendsByPipe } from './shared/orderfriendsby.pipe';
 
 @Component({
   moduleId: module.id,
   selector: 'friends',
   templateUrl: 'friends.component.html',
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES],
+  pipes: [ OrderFriendsByPipe ]
 })
 
 export class FriendsComponent implements OnInit {
 
+  sortBy: string;
   friends: IFriend[] = [];
 
   constructor(private dataService:DataService) {
+    this.sortBy = 'last_name'
   }
 
   ngOnInit() {
@@ -24,6 +28,11 @@ export class FriendsComponent implements OnInit {
       .subscribe((friends:IFriend[]) => {
         this.friends = friends;
       });
+  }
+
+  changeSort(sort:string){
+    this.sortBy = sort;
+    console.log( this.sortBy)
   }
 }
 
